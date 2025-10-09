e06e7ddd5f5b35a116feae0cfeff1b5335aea3b2
Kian Broderick <kianbroderick@Kians-MacBook-Pro.local>
2025-10-08 20:18:53 -0400

    fixed clear_cons and spacing

--- helpers.py
+++ helpers.py
@@ -60,8 +60,17 @@ def get_expr(message: str) -> float:
 
 
 def clear_cons() -> None:
-    rows, columns = shutil.get_terminal_size()
-    os.system("/usr/bin/clear")  # noqa: S605
+    try:
+        rows, columns = shutil.get_terminal_size()
+    except Exception:
+        rows = 80
+    try:
+        if os.name == "nt":
+            os.system("cls")
+        else:
+            os.system("clear")
+    except Exception:
+        pass
     text = Fore.CYAN + "Enter q to quit at any time  " + Fore.RESET
     print(Fore.CYAN + f"{text:>{rows}}" + Fore.RESET)
     print("\n")
@@ -73,7 +82,7 @@ def addition(top: int) -> int:
     b = random.randint(1, top)
     ans = get_int(f"{a} + {b} = ")
     while int(ans) != (a + b):
-        print(Fore.RED + "--   wrong  --\n" + Fore.RESET)
+        print(Fore.RED + "--   wrong   --\n" + Fore.RESET)
         num_wrong += 1
         ans = get_int(f"{a} + {b} = ")
     return num_wrong
@@ -85,7 +94,7 @@ def subtraction(top: int) -> int:
     b = random.randint(1, top)
     ans = get_int(f"{a} - {b} = ")
     while int(ans) != (a - b):
-        print(Fore.RED + "--   wrong  --\n" + Fore.RESET)
+        print(Fore.RED + "--   wrong   --\n" + Fore.RESET)
         num_wrong += 1
         ans = get_int(f"{a} - {b} = ")
     return num_wrong
@@ -103,7 +112,7 @@ def multiplication(top: int) -> int:
     b = random.randint(1, top)
     ans = get_int(f"{a} * {b} = ")
     while int(ans) != (a * b):
-        print(Fore.RED + "--   wrong  --\n" + Fore.RESET)
+        print(Fore.RED + "--   wrong   --\n" + Fore.RESET)
         num_wrong += 1
         ans = get_int(f"{a} * {b} = ")
     return num_wrong
@@ -117,7 +126,7 @@ def division(top: int) -> int:
     quotient = get_int("Quotient = ")
     remainder = get_int("Remainder = ")
     while (quotient, remainder) != divmod(dividend, divisor):
-        print("--   wrong   --\n")
+        print(Fore.RED + "--   wrong   --\n" + Fore.RESET)
         num_wrong += 1
         print(f"{dividend} / {divisor} = ")
         quotient = get_int("Quotient = ")
@@ -130,7 +139,7 @@ def square(top: int) -> int:
     a = random.randint(1, top)
     ans = get_int(f"{a}^2 = ")
     while int(ans) != (a**2):
-        print(Fore.RED + "--   wrong  --\n")
+        print(Fore.RED + "--   wrong   --\n" + Fore.RESET)
         num_wrong += 1
         ans = get_int(f"{a}^2 = ")
     return num_wrong
@@ -144,7 +153,7 @@ def squareroot(top: int) -> int:
     best_answer = rounded - (rounded**2 - a) / (2 * rounded)
     ans = get_expr(f"sqrt({a}) = ")
     while abs(ans - best_answer) > 0.1:
-        print(Fore.RED + "--   wrong  --\n" + Fore.RESET)
+        print(Fore.RED + "--   wrong   --\n" + Fore.RESET)
         num_wrong += 1
         ans = get_expr(f"sqrt({a}) = ")
     print(f"Best answer: {best_answer}")
